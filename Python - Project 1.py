97e94928af39f9ebb6c47e27aa71bf92e2aff1fa
adedamolaonabanjo <47357078+adedamolaonabanjo@users.noreply.github.com>
2019-02-11 15:13:52 +0100

    Add files via upload

---
+++ Python - Project 1.py
@@ -0,0 +1,23 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Users_Name = input("Enter your Name:")
+Enter your Name:Damola
+>>> Users_Age = input ("Enter your Age:")
+Enter your Age:21
+>>> year = 100 - int(Users_Age)
+>>> Age_in_100_years = year + 2019
+>>> print("Your Name is %s and you will be 100 years old in %s." % ( Users_Name ,Age_in_100_years))
+Your Name is Damola and you will be 100 years old in 2098.
+>>> 
+>>> Users_number = input("Choose a number")
+Choose a number
+>>> Users_number = input("Choose a number:")
+Choose a number:3
+>>> if int(Users_number) % 2 == 0:
+    print("you selected an even number")
+else:
+	print("you selected an odd number")
+
+	
+you selected an odd number
+>>> 
